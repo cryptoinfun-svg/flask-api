d03e4414b2d306389f0802465991fa5b21d262dd
cryptoinfun-svg <cryptoinfun@gmail.com>
2026-05-10 20:49:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask
-from models.db import init_db
-from routes.users import users_bp
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
-init_db()
-
-app.register_blueprint(users_bp)
+users = []
 
 @app.route("/")
 def home():
-    return "Backend API running"
+    return "Flask API is running on Replit"
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify({"users": users})
+
+@app.route("/users", methods=["POST"])
+def add_user():
+    data = request.json
+    users.append(data)
+    return jsonify({"message": "added", "users": users})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
